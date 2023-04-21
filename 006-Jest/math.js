8a74b5daa57c04050c88f3c6b47e53d4f1d171d0
Samuel Gomez <contact@samuelgomez.fr>
2023-04-21 07:56:04 +0200

    feat(base): main branch

--- 006-Jest/math.js
+++
@@ -1,8 +0,0 @@
-export const sum = (a, b) => a + b;
-export const subtract = (a, b) => a - b;
-
-export const sumAsync = (...args) =>
-  new Promise(resolve => setTimeout(resolve(sum(...args)), 1000));
-
-export const subtractAsync = (...args) =>
-  new Promise(resolve => setTimeout(resolve(subtract(...args)), 1000));

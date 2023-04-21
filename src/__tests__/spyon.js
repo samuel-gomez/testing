9df87f43e4287f503e63a807565091ab4f05d746
Samuel Gomez <contact@samuelgomez.fr>
2023-04-22 00:24:36 +0200

    feat(spyon) : solution

--- src/__tests__/spyon.js
+++ src/__tests__/spyon.js
@@ -1,9 +1,9 @@
 const thumbWar = require('../thumb-war');
 const utils = require('../utils');
 
-test('returns winner', () => {
-  const originalGetWinner = utils.getWinner;
-  utils.getWinner = jest.fn((p1, p2) => p1);
+test('returns winner spyon', () => {
+  jest.spyOn(utils, 'getWinner');
+  utils.getWinner.mockImplementation((p1, p2) => p1);
 
   const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler');
   expect(winner).toBe('Kent C. Dodds');
@@ -26,5 +26,5 @@ test('returns winner', () => {
     'Ken Wheeler',
   );
 
-  utils.getWinner = originalGetWinner;
+  utils.getWinner.mockRestore();
 });
--- src/no-framework/spyon.js
+++ src/no-framework/spyon.js
@@ -2,17 +2,29 @@ const assert = require('assert');
 const thumbWar = require('../thumb-war');
 const utils = require('../utils');
 
-const fn = impl => {
+const fn = (impl = () => {}) => {
   const mockFn = (...args) => {
     mockFn.mock.calls.push(args);
     return impl(...args);
   };
   mockFn.mock = { calls: [] };
+  // ajout de la méthode d'implémentation
+  mockFn.mockImplementation = newImpl => (impl = newImpl);
+
   return mockFn;
 };
 
-const originalGetWinner = utils.getWinner;
-utils.getWinner = fn((p1, p2) => p1);
+const spyOn = (obj, prop) => {
+  const originalValue = obj[prop];
+  obj[prop] = fn();
+  obj[prop].mockRestore = () => (obj[prop] = originalValue);
+};
+
+spyOn(utils, 'getWinner');
+// const originalGetWinner = utils.getWinner;
+
+// utils.getWinner = fn((p1, p2) => p1);
+utils.getWinner.mockImplementation((p1, p2) => p1);
 
 const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler');
 assert.strictEqual(winner, 'Kent C. Dodds');
@@ -25,4 +37,5 @@ assert.deepStrictEqual(utils.getWinner.mock.calls, [
 assert.strictEqual(utils.getWinner.mock.calls.length, 2);
 
 // cleanup
-utils.getWinner = originalGetWinner;
+//utils.getWinner = originalGetWinner;
+utils.getWinner.mockRestore();

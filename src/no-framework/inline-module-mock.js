c0d8e9e785d26262a899de9ed2ceaa395c9db534
Samuel Gomez <contact@samuelgomez.fr>
2023-04-22 01:44:58 +0200

    feat(module): exercice

---
+++ src/no-framework/inline-module-mock.js
@@ -0,0 +1,36 @@
+const assert = require('assert');
+const thumbWar = require('../thumb-war');
+const utils = require('../utils');
+
+const fn = (impl = () => {}) => {
+  const mockFn = (...args) => {
+    mockFn.mock.calls.push(args);
+    return impl(...args);
+  };
+  mockFn.mock = { calls: [] };
+  mockFn.mockImplementation = newImpl => (impl = newImpl);
+
+  return mockFn;
+};
+
+const spyOn = (obj, prop) => {
+  const originalValue = obj[prop];
+  obj[prop] = fn();
+  obj[prop].mockRestore = () => (obj[prop] = originalValue);
+};
+
+spyOn(utils, 'getWinner');
+utils.getWinner.mockImplementation((p1, p2) => p1);
+
+const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler');
+assert.strictEqual(winner, 'Kent C. Dodds');
+
+assert.deepStrictEqual(utils.getWinner.mock.calls, [
+  ['Kent C. Dodds', 'Ken Wheeler'],
+  ['Kent C. Dodds', 'Ken Wheeler'],
+]);
+
+assert.strictEqual(utils.getWinner.mock.calls.length, 2);
+
+// cleanup
+utils.getWinner.mockRestore();

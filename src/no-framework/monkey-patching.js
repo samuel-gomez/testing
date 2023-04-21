4441e93aa6b47e9229bcac7024d20a9c156897a4
Samuel Gomez <contact@samuelgomez.fr>
2023-04-21 22:42:35 +0200

    feat(call): solution

--- src/no-framework/monkey-patching.js
+++ src/no-framework/monkey-patching.js
@@ -2,11 +2,27 @@ const assert = require('assert');
 const thumbWar = require('../thumb-war');
 const utils = require('../utils');
 
+const fn = impl => {
+  const mockFn = (...args) => {
+    mockFn.mock.calls.push(args);
+    return impl(...args);
+  };
+  mockFn.mock = { calls: [] };
+  return mockFn;
+};
+
 const originalGetWinner = utils.getWinner;
-utils.getWinner = (p1, p2) => p1;
+utils.getWinner = fn((p1, p2) => p1);
 
 const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler');
 assert.strictEqual(winner, 'Kent C. Dodds');
+
+assert.deepStrictEqual(utils.getWinner.mock.calls, [
+  ['Kent C. Dodds', 'Ken Wheeler'],
+  ['Kent C. Dodds', 'Ken Wheeler'],
+]);
+
+assert.strictEqual(utils.getWinner.mock.calls.length, 2);
 
 // cleanup
 utils.getWinner = originalGetWinner;
--- src/thumb-war.js
+++ src/thumb-war.js
@@ -5,7 +5,7 @@ function thumbWar(player1, player2) {
   let player1Wins = 0;
   let player2Wins = 0;
   while (player1Wins < numberToWin && player2Wins < numberToWin) {
-    const winner = utils.getWinner(player1);
+    const winner = utils.getWinner(player1, player2);
     if (winner === player1) {
       player1Wins++;
     } else if (winner === player2) {

360092abde1394ef2d9fa78601c6a769f23e750a
Samuel Gomez <contact@samuelgomez.fr>
2023-04-22 14:43:51 +0200

    feat(lint): exercice

--- src/example.js
+++ src/example.js
@@ -1,13 +1,14 @@
-'use strict'
+'use strict';
 
-const name = 'Freddy'
-typeof name === 'strng'
+const name = 'Freddy';
+typeof name === 'strng';
 
 if (!'serviceWorker' in navigator) {
   // you have an old browser :-(
 }
 
-const greeting = 'hello'
-console.log('${greting} world!')
+const greeting = 'hello';
+console
+  .log('${greting} world!')
 
-[(1, 2, 3)].forEach(x => console.log(x))
+  [(1, 2, 3)].forEach(x => console.log(x));

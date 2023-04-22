b3351bb6db400c64a8c431e2f33d7630cf46b9ad
Samuel Gomez <contact@samuelgomez.fr>
2023-04-22 02:25:38 +0200

    feat(external): from scratch

--- src/no-framework/external-module-mock.js
+++ src/no-framework/external-module-mock.js
@@ -1,24 +1,8 @@
-const fn = (impl = () => {}) => {
-  const mockFn = (...args) => {
-    mockFn.mock.calls.push(args);
-    return impl(...args);
-  };
-  mockFn.mock = { calls: [] };
-  mockFn.mockImplementation = newImpl => (impl = newImpl);
-
-  return mockFn;
-};
-
+require('../__no-framework-mocks__/utils');
 const utilsPath = require.resolve('../utils');
+const mockUtilsPath = require.resolve('../__no-framework-mocks__/utils');
+require.cache[utilsPath] = require.cache[mockUtilsPath];
 
-require.cache[utilsPath] = {
-  id: utilsPath,
-  fileName: utilsPath,
-  loaded: true,
-  exports: {
-    getWinner: fn((p1, p2) => p1),
-  },
-};
 const assert = require('assert');
 const thumbWar = require('../thumb-war');
 const utils = require('../utils');

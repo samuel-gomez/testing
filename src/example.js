6f73b6e028c079cccbd5578b5748895127a74081
Samuel Gomez <contact@samuelgomez.fr>
2023-04-28 23:23:12 +0200

    feat(format): add anomly

--- src/example.js
+++ src/example.js
@@ -1,15 +1,23 @@
-const name = 'Freddy'
+const name = 
+'Freddy'
 typeof name === 'string'
 
 if (!('serviceWorker' in navigator)) {
   // you have an old browser :-(
 }
 
-let greeting = 'hello'
+let greeting = 'hel\lo'
 
 console.log(`${greeting} world!`)
 
-void [1, 2, 3].forEach(x => console.log(x))
+void [1, 2, 3].forEach(     x => console.log(x))
 
 // https://codingbeautydev.com/blog/typescript-cannot-redeclare-block-scoped-variable/
-export {}
+export {
+
+
+
+
+
+
+}
